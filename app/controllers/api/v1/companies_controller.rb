09c682669bcc19eef5044cf5ef65aa6186d87b80
dominikdarnel <dominik.darnel@gmail.com>
2024-05-07 10:32:41 +0200

    Basic Pagination

--- app/controllers/api/v1/companies_controller.rb
+++ app/controllers/api/v1/companies_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::CompaniesController < ApplicationController
   def index
-    companies = Company.all.includes(:deals).order(created_at: :desc)
+    companies = Company.all.includes(:deals).order(created_at: :desc).page(params[:page])
     render json: companies.as_json(include: :deals)
   end
 end
--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,5 @@
 class Company < ApplicationRecord
   has_many :deals
+
+  paginates_per 10
 end
